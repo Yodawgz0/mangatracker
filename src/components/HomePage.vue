97e567979aca7682c2ab1fa784bac6e261a9d7f6
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2021-01-17 02:59:17 +0530

    Removed Redundant Stuff

---
+++ src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+   <div>This is the HomePage Hey People</div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+
+  data: () => ({
+  })
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/HomePage.vue";
 
 export default {
   name: "Home",
