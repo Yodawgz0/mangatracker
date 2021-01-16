da0f1750a5602081d448d396ad7061cba4c8d6de
Blazeblitz276 <sbdevesha1998@gmail.com>
2021-01-16 21:37:40 +0530

    added Vuetify, Router and Store to the branch

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import router from './router'
+import vuetify from './plugins/vuetify';
+import Vuetify from 'vuetify/lib'
+import store from './store/index'
 
-createApp(App).mount('#app')
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  vuetify,
+  store,
+  render: h => h(App)
+}).$mount('#app')
