9a9b06bd5222b67aab98eaa4f55b509dc80298eb
Blazeblitz276 <sbdevesha1998@gmail.com>
2021-01-20 01:33:37 +0530

    Cleaned the Heading and button code stucture and added hover functionality to button

--- src/components/HomePageComponents/Stats.vue
+++ src/components/HomePageComponents/Stats.vue
@@ -1,59 +1,66 @@
 <template>
   <v-container class="stats">
-    <v-row class="stats_padding" align="center" justify="space-around">
-      <div class="stats heading ">
+    <div>
+      <h1 class="text-center stats headerpropeties">
         Track manga series across sites
-      </div>
-    </v-row>
-
-    <v-row align="center" justify="space-around">
-      <div class="stats body">
+      </h1>
+      <p class="text-center stats paragraphpropeties">
         Get the latest releases from supported sites hourly.<br />
         With new sites added monthly, always stay up to date with your favourite
         series
-      </div>
-    </v-row>
+      </p>
+    </div>
 
-    <v-row class="stats_padding" align="center" justify="space-around">
-      <v-btn class="stats checkbutton" color="#3f83f8" large>
-        Check all supported sites
-      </v-btn>
-    </v-row>
+    <div class="text-center ">
+      <v-hover v-slot:default="{ hover }">
+        <v-btn
+          class="stats btnbdrprops txtprops"
+          :color="hover ? '#085ff5' : '#3f83f8'"
+          large
+          depressed
+          dark
+        >
+          Check all supported sites
+        </v-btn>
+      </v-hover>
+    </div>
 
-    <v-row class="stats_padding" justify="center">
-      <v-card min-width="300">
-        <v-card-text class="stats cardtitle">
-          <div class="mangacolorstats">
-            18217
-          </div>
-        </v-card-text>
-        <v-card-subtitle class="stats cardsubtitle">
-          Manga Series Tracked
-        </v-card-subtitle>
-      </v-card>
-      <v-divider vertical></v-divider>
-      <v-card min-width="300">
-        <v-card-text class="stats cardtitle">
-          <div class="mangacolorstats">
-            178766
-          </div>
-        </v-card-text>
-        <v-card-subtitle class="stats cardsubtitle">
-          Manga Entries Created
-        </v-card-subtitle>
-      </v-card>
-      <v-divider vertical></v-divider>
-      <v-card min-width="300">
-        <v-card-text class="stats cardtitle">
-          <div class="mangacolorstats">
-            18
-          </div>
-        </v-card-text>
-        <v-card-subtitle class="stats cardsubtitle">
-          Supported Sites
-        </v-card-subtitle>
-      </v-card>
-    </v-row>
+    <div class="cardsmtopmargin">
+      <v-row class="stats_padding" justify="center">
+        <v-card width="300">
+          <v-card-text>
+            <div class="blue-color stats cardtitle">
+              18217
+            </div>
+          </v-card-text>
+          <v-card-subtitle class="stats cardsubtitle">
+            Manga Series Tracked
+          </v-card-subtitle>
+        </v-card>
+        <v-divider vertical></v-divider>
+        <v-card width="300">
+          <v-card-text>
+            <div class="blue-color stats cardtitle">
+              178766
+            </div>
+          </v-card-text>
+          <v-card-subtitle class="stats cardsubtitle">
+            Manga Entries Created
+          </v-card-subtitle>
+        </v-card>
+        <v-divider vertical></v-divider>
+        <v-card width="300">
+          <v-card-text>
+            <div class="blue-color stats cardtitle">
+              18
+            </div>
+          </v-card-text>
+          <v-card-subtitle class="stats cardsubtitle">
+            Supported Sites
+          </v-card-subtitle>
+        </v-card>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
