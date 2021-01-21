75bb4303689185d6196200f5d14ef030edd1635b
Blazeblitz276 <sbdevesha1998@gmail.com>
2021-01-22 01:04:28 +0530

    Cards view fixed for mobile breakpoints

--- src/components/HomePageComponents/AuthenticationLogins/RegisterCard.vue
+++ src/components/HomePageComponents/AuthenticationLogins/RegisterCard.vue
@@ -1,77 +1,84 @@
 <template>
   <v-card>
-    <v-list class="p1">
-      <v-list-item>
-        <div class="fontbold weight2 fontsize size3 inputtexttitle">Email</div>
-      </v-list-item>
-      <v-list-item>
-        <v-text-field
-          v-model="email"
-          :rules="[rules.email]"
-          autocomplete="email"
-          solo
-          placeholder="you@example.com"
-          required
-          auto-grow
-          type="email"
-          class="inputtextlog"
-          outlined
-          rows="1"
-          dense
-        ></v-text-field>
-      </v-list-item>
-      <v-list-item>
-        <div class="paddingneg fontbold weight2 fontsize size3">Password</div>
-      </v-list-item>
-      <v-list-item class="paddingneg">
-        <v-text-field
-          autocomplete="password"
-          placeholder="Password"
-          class="inputtextlog paddingneg"
-          ref="password"
-          v-model="password"
-          :rules="[rules.password, rules.length(6)]"
-          counter
-          required
-          auto-grow
-          outlined
-          dense
-          rows="1"
-        ></v-text-field>
-      </v-list-item>
-      <v-list-item>
-        <div class="paddingneg fontbold weight2 fontsize size3">
-          Password Confirmation
-        </div>
-      </v-list-item>
-      <v-list-item class="paddingneg">
-        <v-text-field
-          autocomplete="password"
-          placeholder="Password confirmation"
-          class="inputtextlog"
-          v-validate="'required|confirmed:password'"
-          v-model="passwordconf"
-          counter
-          required
-          auto-grow
-          outlined
-          dense
-          rows="1"
-        ></v-text-field>
-      </v-list-item>
-      <v-list-item>
-        <v-btn block elevation="2" medium color="#1c64f2">
-          <div class="inputsubmittext font">Register</div></v-btn
-        >
-      </v-list-item>
-      <v-list-item> <v-divider></v-divider></v-list-item>
-      <v-list-item class="paddingneg space-left long">
-        <div class="fontsize size3">Already have an account?</div>
-        <div style="padding-left: 5px" class="fontsize size3 fontbold weight2">
-          Sign In
-        </div>
-      </v-list-item>
-    </v-list>
+    <v-form>
+      <v-list class="p1">
+        <v-list-item>
+          <div class="fontbold weight2 fontsize size3 inputtexttitle">
+            Email
+          </div>
+        </v-list-item>
+        <v-list-item>
+          <v-text-field
+            v-model="email"
+            :rules="[rules.email]"
+            autocomplete="email"
+            solo
+            placeholder="you@example.com"
+            required
+            auto-grow
+            type="email"
+            class="inputtextlog"
+            outlined
+            rows="1"
+            dense
+          ></v-text-field>
+        </v-list-item>
+        <v-list-item>
+          <div class="paddingneg fontbold weight2 fontsize size3">Password</div>
+        </v-list-item>
+        <v-list-item class="paddingneg">
+          <v-text-field
+            autocomplete="password"
+            placeholder="Password"
+            class="inputtextlog paddingneg"
+            ref="password"
+            v-model="password"
+            :rules="[rules.password, rules.length(6)]"
+            counter
+            required
+            auto-grow
+            outlined
+            dense
+            rows="1"
+          ></v-text-field>
+        </v-list-item>
+        <v-list-item>
+          <div class="paddingneg fontbold weight2 fontsize size3">
+            Password Confirmation
+          </div>
+        </v-list-item>
+        <v-list-item class="paddingneg">
+          <v-text-field
+            autocomplete="password"
+            placeholder="Password confirmation"
+            class="inputtextlog"
+            v-validate="'required|confirmed:password'"
+            v-model="passwordconf"
+            counter
+            required
+            auto-grow
+            outlined
+            dense
+            rows="1"
+          ></v-text-field>
+        </v-list-item>
+        <v-list-item>
+          <v-btn block elevation="2" medium color="#1c64f2">
+            <div class="inputsubmittext font">Register</div></v-btn
+          >
+        </v-list-item>
+        <v-list-item> <v-divider></v-divider></v-list-item>
+        <v-list-item class="paddingneg space-left long">
+          <div class="fontsize size3">Already have an account?</div>
+          <div
+            style="padding-left: 5px"
+            class="fontsize size3 fontbold weight2"
+          >
+            Sign In
+          </div>
+        </v-list-item>
+      </v-list>
+    </v-form>
   </v-card>
 </template>
 
